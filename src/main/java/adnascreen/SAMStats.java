77aac4dbb08a8e0770af4186f187569989d4a4cd
Matthew Mah <mmah@broadinstitute.org>
2017-05-05 16:52:33 -0400

    Add mapping quality and coverage to SAMStats. Use Apache commons command line library, which requires change calling

--- src/main/java/adnascreen/SAMStats.java
+++ src/main/java/adnascreen/SAMStats.java
@@ -8,6 +8,11 @@ import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.commons.math3.stat.Frequency;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -28,7 +33,7 @@ public class SAMStats {
 	private Map<String, String> referenceNamesToTargetClass;
 	SampleSetsCounter counter;
 
-	public SAMStats(String filename, JSONObject targets) throws IOException{
+	public SAMStats(String filename, JSONObject targets, int minimumMappingQuality) throws IOException{
 		lengthHistogram = new Frequency();
 		IndexAndBarcodeKey key = ReadMarkDuplicatesStatistics.keyFromFilename(filename);
 		counter = new SampleSetsCounter();
@@ -60,16 +65,21 @@ public class SAMStats {
 				try{
 					SAMRecord record = i.next();
 					if(!record.getReadUnmappedFlag()){ // read is mapped
-						// record length
-						int readLength = record.getReadLength();
-						lengthHistogram.addValue(readLength);
-						// count alignments to reference sequences 
-						String referenceName = record.getReferenceName();
-						String targetClass = referenceNamesToTargetClass.get(referenceName);
-						if(targetClass != null){
-							counter.increment(key, targetClass);
+						int mappingQuality = record.getMappingQuality();
+						if(mappingQuality >= minimumMappingQuality){
+							// record length
+							int readLength = record.getReadLength();
+							lengthHistogram.addValue(readLength);
+							// count alignments to reference sequences 
+							String referenceName = record.getReferenceName();
+							String targetClass = referenceNamesToTargetClass.get(referenceName);
+							if(targetClass != null){
+								counter.increment(key, targetClass);
+								String targetCoverageLabel = targetClass + "-coverageLength";
+								counter.add(key, targetCoverageLabel, readLength);
+							}
+							//System.out.println(referenceName + "\t" + targetClass);
 						}
-						//System.out.println(referenceName + "\t" + targetClass);
 					}
 				} catch (SAMFormatException e){
 					System.err.println(e);
@@ -80,18 +90,25 @@ public class SAMStats {
 		}
 	}
 	
-	public static void main(String []args) throws IOException{
-		String filename = args[0];
-		JSONObject targets = new JSONObject(args[1]);
-		String histogramFilename = null;
-		if(args.length >= 2)
-			histogramFilename = args[2];
+	public static void main(String []args) throws IOException, ParseException{
+		CommandLineParser parser = new DefaultParser();
+		Options options = new Options();
+		options.addRequiredOption("f", "filename", true, "input SAM/BAM filename");
+		options.addRequiredOption("t", "targets", true, "JSON object for targets: key is label, value is reference name or array of reference names");
+		options.addOption("l", "length-histogram", true, "filename for length distribution histogram");
+		options.addOption("q", "mapping-quality", true, "minimum Phred score mapping quality");
+		CommandLine commandLine	= parser.parse( options, args );
+		
+		String filename = commandLine.getOptionValue('f');
+		JSONObject targets = new JSONObject(commandLine.getOptionValue('t'));
+		String histogramFilename = commandLine.getOptionValue('h');
+		int minimumMappingQuality = Integer.valueOf(commandLine.getOptionValue('q', "0"));
 		
 		// parse the second argument as JSON
 		// each key is a label
 		// value is the reference name to associate with that label
 		// or value is array of reference names to associate with label
-		SAMStats stats = new SAMStats(filename, targets);
+		SAMStats stats = new SAMStats(filename, targets, minimumMappingQuality);
 		
 		if(histogramFilename != null){
 			try(
